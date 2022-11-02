40c65bf1f79a7c7c59b0b9e52e39633c0a5d0d96
Nikodem Majzner <nikodemmajzner@gmail.com>
2022-11-02 08:11:07 +0100

    BETTER VERSION!

--- main.py
+++ main.py
@@ -1,9 +1,66 @@
-from random import random
+import random
 
-words = ["jabłko;Logo apple", "jeansy;Spodnie Jobs'a",
+wordsList = ["jabłko;Logo apple", "jeansy;Spodnie Jobs'a",
          "laptop;słowo, które automatycznie wypada ze słownika w momencie zakupu macbooka",
-         "ARM;architektura procesora"]
+         "arm;architektura procesora"]
+letters = "a,b,c,d,f,g,h,i,j,k,l,ł,m,n,o,q,p,r,s,ś,t,u,ó,w,y,z,ź,ż"
+lettersTab = letters.split(",")
+knownLettersTab = []
 
-nick = input("Podaj swój nick: ")
-print(words[random()*3])
-print(f"Witaj {nick}!")
+
+helper1 = wordsList[random.randrange(0, 4)].split(";")
+word = helper1[0]
+description = helper1[1]
+lifes = 5
+win = False
+
+
+def writeLifes():
+    print("Szanse: ", end="")
+    x = 0
+    while x < lifes:
+        print("<3 ", end="")
+        x = x + 1
+    print()
+
+hiddenWord = []
+
+def firstWord():
+    for x in word:
+        hiddenWord.append("_")
+    for x in hiddenWord:
+        print(x, end="")
+    print()
+
+
+def printWord(letter):
+    isLetter = False
+    y = 0
+    for x in word:
+        if x==letter:
+            hiddenWord[y]=x
+            isLetter = True
+        y = y + 1
+
+    for x in hiddenWord:
+        print(x, end="")
+    print()
+    return isLetter
+
+firstWord()
+writeLifes()
+
+while lifes > 0 and win == False:
+    print(f"Podpowiedź: {description}")
+    if printWord(input("Podaj literę: ").lower()):
+        print("Poprawne!")
+    else:
+        lifes = lifes - 1
+    if("".join(hiddenWord) == word):
+        win = True
+        print("WYGRYWASZ!")
+
+    print()
+    writeLifes()
+if(win == False):
+    print("Przegałeś :c")
